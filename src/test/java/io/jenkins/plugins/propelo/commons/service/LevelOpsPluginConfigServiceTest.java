08e800239051f5d8ec533ea09e36b24549162d1a
Ashish Duthade <ashish.duthade@harness.io>
2024-08-09 14:54:47 +0530

    feat: [SEI-7028]: Jenkins Configuration as Code

--- src/test/java/io/jenkins/plugins/propelo/commons/service/LevelOpsPluginConfigServiceTest.java
+++ src/test/java/io/jenkins/plugins/propelo/commons/service/LevelOpsPluginConfigServiceTest.java
@@ -3,33 +3,40 @@ package io.jenkins.plugins.propelo.commons.service;
 import io.jenkins.plugins.propelo.commons.models.ApplicationType;
 import io.jenkins.plugins.propelo.job_reporter.plugins.PropeloPluginImpl;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
 
 import java.io.File;
 
+import static io.jenkins.plugins.propelo.commons.models.PropeloJobReporterConfiguration.CONFIGURATION;
+
 public class LevelOpsPluginConfigServiceTest {
+    @Rule
+    public JenkinsRule r = new JenkinsRule();
+
     @Test
     public void testApplicationType() throws Exception {
 
         PropeloPluginImpl propeloPlugin = new PropeloPluginImpl();
 
-        propeloPlugin.setApplicationType("SEI-Harness-PROD1");
+        CONFIGURATION.setApplicationType(ApplicationType.fromString("SEI-Harness-PROD1"));
         ApplicationType applicationType = PropeloPluginImpl.getInstance().getApplicationType();
         Assert.assertEquals(applicationType, ApplicationType.SEI_HARNESS_PROD1);
 
-        propeloPlugin.setApplicationType("SEI-Harness-PROD3");
+        CONFIGURATION.setApplicationType(ApplicationType.fromString("SEI-Harness-PROD3"));
         applicationType = PropeloPluginImpl.getInstance().getApplicationType();
         Assert.assertEquals(applicationType, ApplicationType.SEI_HARNESS_PROD3);
 
-        propeloPlugin.setApplicationType("SEI-Legacy");
+        CONFIGURATION.setApplicationType(ApplicationType.fromString("SEI-Legacy"));
         applicationType = PropeloPluginImpl.getInstance().getApplicationType();
         Assert.assertEquals(applicationType, ApplicationType.SEI_LEGACY);
 
-        propeloPlugin.setApplicationType("SEI-Legacy-EU");
+        CONFIGURATION.setApplicationType(ApplicationType.fromString("SEI-Legacy-EU"));
         applicationType = PropeloPluginImpl.getInstance().getApplicationType();
         Assert.assertEquals(applicationType, ApplicationType.SEI_LEGACY_EU);
 
-        propeloPlugin.setApplicationType("SEI-Harness-PROD2");
+        CONFIGURATION.setApplicationType(ApplicationType.fromString("SEI-Harness-PROD2"));
         applicationType = PropeloPluginImpl.getInstance().getApplicationType();
         Assert.assertEquals(applicationType, ApplicationType.SEI_HARNESS_PROD2);
     }
